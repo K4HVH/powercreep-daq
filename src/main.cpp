89e8d74439bf80869672f243466f1e1803a5d149
K4HVH <bronercast@gmail.com>
2025-12-24 20:56:32 +1100

    Init PlatformIO

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+  Serial.begin(921600);
+
+  Serial.print("Result of myFunction(2, 3): ");
+  Serial.println(result);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello, World!");
+  delay(1000);
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
